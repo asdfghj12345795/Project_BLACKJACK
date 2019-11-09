47fefad81979b9aad2538224d1741f6a84e3d711
asdfghj12345795 <55428385+asdfghj12345795@users.noreply.github.com>
2019-11-09 22:21:09 +0900

    11.09_submit

--- Offer the cards and play round/Offer the cards and play round.c
+++ Offer the cards and play round/Offer the cards and play round.c
@@ -21,7 +21,7 @@ int PLAY_NUMBER;//fix the play number
 int N_ROUND=1; //represent the count of round
 int N_DOLLAR = 50;
 int Card_set[N_CARD] = {1,2,3,4,5,6,7,8,9,10,10,10,10,1,2,3,4,5,6,7,8,9,10,10,10,10,1,2,3,4,5,6,7,8,9,10,10,10,10,1,2,3,4,5,6,7,8,9,10,10,10,10}; //total card is 52
-int Player_Card[N_MAX_USER];
+int Player_Card[N_MAX_USER][N_MAX_CARD];
 int Dealer_card[N_MAX_CARD]; 
 
 int getIntegerInput(void) // the function that protect to input
@@ -58,30 +58,57 @@ int PLAY_NUM() // input the sum of players
 printf("---> card is mixed and put into the tray\n");
 return 0;
 }
-extern int Player_Card[N_MAX_USER];;
+extern int Player_Card[N_MAX_USER][N_MAX_CARD];
 
-int Card_Offer()
+int Card_Offer_1()//Give the card each player.
 {
 		int Player_Card[PLAY_NUMBER];
 		int i;
-		
+		int j;
+		int h;//function that set for player to give two cards. 
 		do{
-		
-		for(i=1;i=2;i++)
+		for(i=1;i<3;i++)
 		{
+			srand((unsigned)time(NULL));
 			Dealer_card[i]=rand()%52;
-		}
+		} } while(Dealer_card[1]=Dealer_card[2]);
 		
-		for(i=1;i<=PLAY_NUMBER;i++) //set the card for player
-		{
-				Player_Card[i]=rand()%52;
-		} 
+		do
+		{	
+			for(h=1;h<3;h++) //function that set for player to give two cards.
+			{
+				for(j=1,h=1;j<PLAY_NUMBER,h<3;j++,h++)
+			{
+				srand((unsigned)time(NULL));
+				Player_Card[h][j]=rand()%52;
+			}
+		}
 		
 		}while(Dealer_card[1]=Dealer_card[2]||Dealer_card[1]=Player[1]||Dealer_card[1]=Player[2]||Dealer_card[1]=Player[3]||Dealer_card[1]=Player[4]||Dealer_card[1]=Player[5]||Dealer_card[2]=Player[1]
 ||Dealer_card[2]=Player[2]||Dealer_card[2]=Player[3]||Dealer_card[2]=Player[4]||Dealer_card[2]=Player[5]||Player[1]=Player[2]||Player[1]=Player[3]||Player[1]=Player[4]||Player[1]=Player[5]||Player[2]=Player[3]||Player[2]=Player[4||Player[2]=Player[5]||Player[3]=Player[4]||Player[3]=Player[5]||Player[4]=Player[5]);
 		//Player number is limited '5'. So the limination arrary number is '5'. 
 		//If the card is set to Player 1, that card can't set to oher players.
-		
-		
+			
+		return 0;
+}
 
+int Card_SHOW_1()
+{
+	int i;
+	int j;
+	
+	printf("------------- CARD OFFERING ---------------\n");
+	Card_Offer_1();
+
+	printf(" delear : X %d", Dealer_card[2]);
+	printf(" -> you : %d %d",Player_Card[1][1], Player_Card[2][1]);	
+	
+	for(j=1;i<3;j++)
+	{
+		for(i=2;i<=PLAY_NUMBER;i++)
+	{
+	printf(" -> player %d : % %", i, Player_Card[j][i]);
+	}
+}
+	return 0;
 }
